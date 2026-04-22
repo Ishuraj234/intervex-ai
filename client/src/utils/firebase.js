d7b7fa7f8d79f9cc01e3378609bc6f8cbefda314
Ishuraj234 <ishur8721@gmail.com>
2026-04-22 19:22:16 +0530

    firebase

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -1,6 +1,26 @@
 
+// import { initializeApp } from "firebase/app";
+// import {getAuth, GoogleAuthProvider} from "firebase/auth"
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+//   authDomain: "interview-8ae09.firebaseapp.com",
+//   projectId: "interview-8ae09",
+//   storageBucket: "interview-8ae09.firebasestorage.app",
+//   messagingSenderId: "1022574009932",
+//   appId: "1:1022574009932:web:5cd51cbd881627570d1ee9"
+// };
+
+// const app = initializeApp(firebaseConfig);
+
+// const auth = getAuth(app);
+
+// const provider = new GoogleAuthProvider()
+
+// export {auth , provider}
+
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
   authDomain: "interview-8ae09.firebaseapp.com",
@@ -13,7 +33,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
-
 const provider = new GoogleAuthProvider();
 
-export {auth , provider};
+const provider = new GoogleAuthProvider()
+
+export {auth , provider}
